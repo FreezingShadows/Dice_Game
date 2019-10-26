315f5a653bd040574fed408513addb2fc43a361e
FreezingShadows <56938596+FreezingShadows@users.noreply.github.com>
2019-10-26 14:39:08 -0400

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,16 @@
+<header>
+    <div>
+
+    <h1 class="titleText">Welcome to my Dice Game!</h1>
+
+    </div>
+    <div>
+
+        <nav>
+            <ul>
+                <li><a href="/Dice_Game/round.php">Play Game</a></li>
+                <li><a href="/Dice_Game/leaderboard.php">View Leaderboard</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
