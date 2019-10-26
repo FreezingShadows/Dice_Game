5e828589e35e0bc4a7fd9b14a3a89f24a74b6fd2
FreezingShadows <56938596+FreezingShadows@users.noreply.github.com>
2019-10-26 14:42:22 -0400

    Create Insert_Leaderboard.sql

---
+++ Insert_Leaderboard.sql
@@ -0,0 +1,79 @@
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('TopScore', 8);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('Lucky777', 7);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('CoolGuy', 6);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('IGotFive', 5);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('555', 5);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('JDoe', 5);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('IWasHere', 4);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('4IsCool', 4);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('SomeGuy', 4);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('FooBar', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('MPython', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('Delta333', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('J_Random', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('KSoze', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('F.Bloggs', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('Anon', 3);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('NotLucky', 2);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('OtherGuy', 2);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('Anon', 2);
+
+INSERT INTO Leaderboard (display_name, score)
+VALUES 
+('DeadLast', 2);
